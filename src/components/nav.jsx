02aa858b3fd07e48144b8006a50c54b5bf5fe90a
arshdeep singh <arshdeep4909@gmail.com>
2022-04-10 18:12:35 -0400

    built the nav bar.

---
+++ src/components/nav.jsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import background from "../image/background.webp";
+
+const Nav = () => {
+  return (
+    <NavBar>
+      <Logo>
+        <p href="#">Dev</p>
+      </Logo>
+      <NavList>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Resume</li>
+        <li>Contact</li>
+      </NavList>
+    </NavBar>
+  );
+};
+
+const NavBar = styled.nav`
+  display: flex;
+  font-family: "Grape Nuts", cursive;
+  justify-content: space-between;
+  background-image: url("https://wallpapertag.com/wallpaper/full/8/4/6/252350-free-black-textured-background-2560x1600-for-samsung.jpg");
+`;
+const Logo = styled.div`
+  p {
+    text-decoration: none;
+    border: none;
+    border-radius: 2rem;
+    font-size: 3rem;
+    padding: 1px 10px;
+    margin: 1rem 3rem;
+    color: white;
+    transition: all 0.3s ease;
+  }
+  p:hover {
+    cursor: pointer;
+    color: #f8735b;
+    font-weight: bold;
+  }
+`;
+
+const NavList = styled.ul`
+  display: flex;
+  padding-top: 6px;
+  height: 100%;
+  list-style: none;
+  color: white;
+  font-size: 1.5rem;
+  font-weight: lighter;
+  li {
+    margin-right: 3rem;
+    &:hover {
+      cursor: pointer;
+      color: tomato;
+    }
+  }
+`;
+
+export default Nav;
+
+// font-family: 'Poppins', sans-serif;
